e64dc7e72cd4d3358c7d1c614e14d880c9ace579
Luís Felype <luisfelype.fioravanti@gmail.com>
2018-10-08 20:21:38 -0300

    Estrutura Main, Matheus olha os comentários no Main

--- src/Main/Executavel.java
+++ src/Main/Executavel.java
@@ -3,32 +3,59 @@ package Main;
 import java.util.Scanner;
 
 public class Executavel {
-	
+
 	public static void insere(ListaContas listaConta) {
 		System.out.println("Digite o numero da mesa :");
-		Scanner scanner = new Scanner (System.in);
+		Scanner scanner = new Scanner(System.in);
 		int i = scanner.nextInt();
-		//carrega e mostra Lista de Pratos
+		// carrega e mostra Lista de Pratos
 		listaConta.insere(i);
-		
+
 	}
+
 	public static void imprime(ListaContas listaConta) {
 		System.out.println("Imprime");
 		listaConta.imprime();
 	}
+
 	public static void main(String[] args) {
 		ListaContas listaConta = new ListaContas();
-		//aqui tem que carregar o HashMap de Contas do Arquivo pra essa ListaConta
-		System.out.println("Digite a opcao desejada:");
-		System.out.println("1-Insere:");
-		Scanner scanner = new Scanner (System.in);
-		int i = scanner.nextInt();
-		if(i == 1) {
-			for(int j=0;j<2;j++) {
-				insere(listaConta);
+		// aqui tem que carregar o HashMap de Contas do Arquivo pra essa ListaConta
+		int loop = 0;
+		Scanner scanner = new Scanner(System.in);
+		while (loop != 9) {
+			System.out.println("Bem Vindo ao Restaurante do Hudson!");
+			System.out.println("Digite a opcao desejada:");
+			System.out.println("1- Inserir um novo pedido");
+			System.out.println("2- Listar as contas atuais:");
+			System.out.println("3- Fechar um conta existente");
+			System.out.println("4- Atualizar Menu");
+			System.out.println("9- Encerrar Programa");
+			
+
+			int op = scanner.nextInt();
+			
+			switch(op) {
+				case(1):
+					System.out.println("Digite a quantidade de pedidos que deseja fazer:");
+					int qtdPedidos = scanner.nextInt();
+					for (int i = 0; i < qtdPedidos; i++) {
+						insere(listaConta);
+					}
+					break;
+				case(2):
+					imprime(listaConta);
+					break;
+				case(3):
+					//fechaConta(listaConta);
+					break;
+				case(4):
+					//atualizaMenu(listaConta);
+					break;
 			}
-			imprime(listaConta);
 		}
+		//aqui tem que gravar a lista no arquivo, pq o programa vai fechar.
+		System.out.println("Volte Sempre!");
 	}
 
 }
